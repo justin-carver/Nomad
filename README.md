01ee7a91e01e8a1ed21e3efef23325e30fc9b8db
Justin Carver <justin.carver@hotmail.com>
2022-09-28 03:31:25 -0500

    Prototyping login route.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ## Nomad
 
-Nomad is a simple, lightweight, customizable, blogging system built on Next.js and the Mantine component library.
+Nomad is a simple, lightweight, customizable, blogging system built on Next.js and the Mantine component library. The core post database systems is built off of postgreSQL.
---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+import { Paper, Center, Container } from '@mantine/core';
+import styles from '../styles/login.module.css';
+
+const Login = () => {
+	return (
+		<Center className={styles['Login']}>
+			<Container className={styles['Login__container']}>Testing</Container>
+		</Center>
+	);
+};
+
+export default Login;
